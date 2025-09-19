203b7a5631972e49e904acce4491f7865790bc81
Thiago <thiago.deny@gmail.com>
2025-09-19 16:58:13 -0300

    layout e sidebar + font adicionados (primeiro commit)

---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+const ProductsPage = () => {
+  return (
+    <>
+      <h1>Products Page page</h1>
+    </>
+  );
+};
+
+export default ProductsPage;
---
+++ app/products/product-details/page.tsx
@@ -0,0 +1,9 @@
+const ProductDetailsPage = () => {
+  return (
+    <>
+      <h1>Products details page</h1>
+    </>
+  );
+};
+
+export default ProductDetailsPage;
