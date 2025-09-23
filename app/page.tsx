0b8fa7d5b7fc33e0ce990247210292f66bb3e87a
Thiago <thiago.devy@gmail.com>
2025-09-22 22:13:27 -0300

    feat: padrao do combobox para criação na tela de vendas

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return (
     <>
-      <h1>Dashboard page</h1>
+      <div className="m-8 my-8 w-full space-y-8 rounded-lg bg-white p-8">
+        {/* Esquerda */}
+        <div className="flex w-full items-center justify-between">
+          <div className="space-y-1">
+            <span className="text-xs font-semibold text-slate-500">
+              Gestão Geral
+            </span>
+            <h2 className="text-xl font-semibold">Dashboard</h2>
+          </div>
+        </div>
+        {/* Direita */}
+      </div>
     </>
   );
 };
